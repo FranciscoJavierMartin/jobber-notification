00efe783c19f7f396d43b59d0edecebaa58d3af6
Francisco <javiermartincuenca@gmail.com>
2024-01-21 13:12:45 +0100

    Send orders email

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,7 @@ import http from 'http';
 import { Application } from 'express';
 import { Logger } from 'winston';
 import { Channel } from 'amqplib';
-import {
-  IEmailMessageDetails,
-  winstonLogger,
-} from '@franciscojaviermartin/jobber-shared';
+import { winstonLogger } from '@franciscojaviermartin/jobber-shared';
 import { config } from '@notifications/config';
 import { healthRoutes } from '@notifications/routes';
 import { checkConnection } from '@notifications/elasticsearch';
@@ -15,8 +12,6 @@ import { createConnection } from '@notifications/queues/connection';
 import {
   consumeAuthEmailMessages,
   consumeOrderEmailMessages,
-  exchangeAuthName,
-  routingAuthKey,
 } from '@notifications/queues/email.consumer';
 
 const SERVER_PORT: number = config.SERVER_PORT;
@@ -38,21 +33,6 @@ async function startQueues(): Promise<void> {
   const emailChannel: Channel = (await createConnection())!;
   await consumeAuthEmailMessages(emailChannel);
   await consumeOrderEmailMessages(emailChannel);
-
-  const verifyLink = `${config.CLIENT_URL}/confirm_email?token=1234`;
-  const messageDetails: IEmailMessageDetails = {
-    receiverEmail: config.SENDER_EMAIL,
-    verifyLink,
-    template: 'verifyEmail',
-  };
-
-  await emailChannel.assertExchange(exchangeAuthName, 'direct');
-  const messageAuth = JSON.stringify(messageDetails);
-  emailChannel.publish(
-    exchangeAuthName,
-    routingAuthKey,
-    Buffer.from(messageAuth)
-  );
 }
 
 function startElasticSearch(): void {
